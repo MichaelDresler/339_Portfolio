d46cf32c98c5990232c7b70c5e9ad918fbb3ddda
Michael Dresler <michaeldresler@hotmail.com>
2023-11-24 01:58:28 -0800

    setting up home page
    fluid type + layout

---
+++ js/nav.js
@@ -0,0 +1,15 @@
+const primaryNav = document.querySelector(".primary-nav")
+const navToggle = document.querySelector(".mobile-nav-toggle")
+document.getElementById("navbar").style.display = "flex";
+
+
+navToggle.addEventListener("click", () =>{
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if(visibility === "false"){
+        primaryNav.setAttribute('data-visible',true) 
+    }
+    else if(visibility==="true"){
+        primaryNav.setAttribute('data-visible',false)
+    }
+});
